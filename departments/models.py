ff47d5b587a693eaae67d18d318c26ce6be472ee
Brunel Bodji <brunelb@192.168.1.9>
2026-04-20 17:14:42 +0000

    implémentation de l'auth , des rôles et des dashboards

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
