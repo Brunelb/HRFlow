8a1b09e7207e62757c7c1c717839c3d28a36d5f7
Brunel Bodji <brunelb@192.168.1.9>
2026-04-21 22:02:00 +0000

    test sur l'app account pour mieux comprendre la gestion des rôles

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
         ('employee', 'Employee'),
     )
 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='admin')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
 
     def __str__(self):
         return self.username
